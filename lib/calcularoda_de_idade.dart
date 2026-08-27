663937b44dbf9887fa60da6bdd72f4d6980b2811
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 13:42:46 -0300

    Trabalhando com String

--- lib/calcularoda_de_idade.dart
+++ lib/calcularoda_de_idade.dart
@@ -1,4 +1,15 @@
 void main(List<String> arguments) {
+// 4. Calculadora de idade
+// Crie duas variáveis:
+
+// Ano atual;
+
+// Ano de nascimento.
+
+// Calcule e mostre a idade aproximada da pessoa.
+
+// Desafio adicional: utilize uma constante para representar o ano atual.
+
   const anoAtual = 2026;
   var anoNascimento = 1992;
   var idade = anoAtual - anoNascimento;
---
+++ lib/trabalhando_com_string.dart
@@ -0,0 +1,25 @@
+void main(List<String> arguments) {
+// 5. Trabalhando com Strings
+// Crie uma variável contendo seu nome completo e mostre:
+
+// O nome completo;
+
+// O nome em letras maiúsculas;
+
+// O nome em letras minúsculas;
+
+// A quantidade de caracteres;
+
+// Se o nome contém a letra a.
+
+// Conceitos: String e seus métodos.
+
+  var nome = "Flávio Gimenes Spinelli";
+
+  print(nome);
+  print(nome.toUpperCase());
+  print(nome.toLowerCase());
+  print(nome.length);
+  print(nome.contains("a"));
+
+}
