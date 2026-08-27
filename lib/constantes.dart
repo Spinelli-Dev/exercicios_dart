06e14b0372a3ef62b72a75643aee60a5afa5d9a1
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 13:59:17 -0300

    Constantes e sua imutabilidade

---
+++ lib/constantes.dart
@@ -0,0 +1,28 @@
+void main(List<String> arguments) {
+// 8. Constantes
+// Crie constantes para representar:
+
+// O valor de PI;
+
+// O número máximo de alunos de uma turma;
+
+// O nome de uma escola.
+
+// Depois, tente alterar uma das constantes e observe o erro apresentado.
+
+// Conceitos: const e imutabilidade.
+
+  const pi = 3.14;
+  const numeroMaxAlunos = 30;
+
+  print(pi);
+  print(numeroMaxAlunos);
+
+  // pi = 3.15;
+  // numeroMaxAlunos = 31;
+
+  /*
+  Constant variables can't be assigned a value after initialization.
+  Try removing the assignment, or remove the modifier 'const' from the variable.  
+  */
+}
