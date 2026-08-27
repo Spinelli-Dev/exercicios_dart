6bc4e5ca070a5cc107f13402c7579eee2df112cd
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 13:33:45 -0300

    Conversão de temperatura

---
+++ lib/conversao_temperatura.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+  // Crie uma variável contendo uma temperatura em Celsius e converta para Fahrenheit.
+  // Fahrenheit = (Celsius x 9 / 5) + 32
+
+  var celsius = 38.0;
+  var fahrenheit = (celsius * 9 / 5) + 32;
+
+  print("A temperatura $celsiusº celsius é igual a $fahrenheitº fahrenheit.");
+}
