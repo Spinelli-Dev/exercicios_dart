44f7230c8707789bc215088bbd3e8161965ffe2a
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 11:48:55 -0300

    1. Apresentação pessoal

---
+++ bin/exercicios_dart.dart
@@ -0,0 +1,27 @@
+import 'package:exercicios_dart/exercicios_dart.dart' as exercicios_dart;
+
+void main(List<String> arguments) {
+// 1. Apresentação pessoal
+// Crie variáveis para armazenar:
+
+// Nome;
+
+// Idade;
+
+// Altura;
+
+// Se está estudando Dart;
+
+// Cidade onde mora.
+
+// Depois, exiba uma apresentação completa no console.
+
+var nome = "Flávio";
+var idade = "34";
+var altura = 1.70;
+var estudandoDart = true;
+var cidade = "Ribeirão Preto";
+
+print("Meu nome é $nome, tenho $idade m anos, tenho $altura e moro em $cidade");
+print("Estou estudando Dart: $estudandoDart");
+}
