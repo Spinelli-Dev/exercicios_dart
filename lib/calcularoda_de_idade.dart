5343ac40f30bb3b8b600af7f9efd3b53a7e88a9f
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 13:37:41 -0300

    Calculadora de Idade

---
+++ lib/calcularoda_de_idade.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  const anoAtual = 2026;
+  var anoNascimento = 1992;
+  var idade = anoAtual - anoNascimento;
+
+  print("Você têm $idade anos de idade.");
+
+}
