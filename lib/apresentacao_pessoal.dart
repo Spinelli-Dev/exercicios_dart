549842f44660c41cf65f2dab8883abfd7df5b8f9
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 13:28:28 -0300

    Operações Matemáticas

---
+++ lib/apresentacao_pessoal.dart
@@ -0,0 +1,25 @@
+void main(List<String> arguments) {
+// 1. Apresentação pessoal
+// Crie variáveis para armazenar:
+
+// Nome;
+
+// Idade;
+
+// Altura;
+
+// Se está estudando Dart;
+
+// Cidade onde mora.
+
+// Depois, exiba uma apresentação completa no console.
+
+var nome = "Flávio";
+var idade = "34";
+var altura = 1.70;
+var estudandoDart = true;
+var cidade = "Ribeirão Preto";
+
+print("Meu nome é $nome, tenho $idade anos, tenho $altura m e moro em $cidade.");
+print("Estou estudando Dart: $estudandoDart");
+}
---
+++ lib/operacoes_matematicas.dart
@@ -0,0 +1,30 @@
+void main(List<String> arguments) {
+// 2. Operações matemáticas
+// Crie duas variáveis numéricas e exiba:
+
+// Soma;
+
+// Subtração;
+
+// Multiplicação;
+
+// Divisão;
+
+// Resto da divisão.
+
+// Conceitos: variáveis, operadores matemáticos e double.
+
+var num1 = 10.0;
+var num2 = 5.0;
+
+var soma = num1 + num2;
+var sub = num1 - num2;
+var mult = num1 * num2;
+var divi = num1  / num2;
+
+print("A soma de $num1 + $num2 é: $soma");
+print("A subtração de $num1 - $num2 é: $sub");
+print("A multiplicação de $num1 * $num2 é: $mult");
+print("A divisão de $num1 / $num2 é: $divi");
+
+}
