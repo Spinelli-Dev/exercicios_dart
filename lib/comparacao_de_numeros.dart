94261a842829c90cddf7dce0f0bd2f12cdf4be82
Spinelli-Dev <225873656+Spinelli-Dev@users.noreply.github.com>
2026-08-27 13:50:36 -0300

    Comparação de números

---
+++ lib/comparacao_de_numeros.dart
@@ -0,0 +1,22 @@
+void main(List<String> arguments) {
+// 6. Comparação de números
+// Crie dois números e exiba os resultados das seguintes comparações:
+
+// Primeiro número é maior que o segundo;
+
+// Primeiro número é menor que o segundo;
+
+// Os dois números são iguais;
+
+// Os dois números são diferentes.
+
+// Conceitos: comparação e valores booleanos.
+
+  var num1 = 10;
+  var num2 = 5;
+
+  print("$num1 > $num2:  ${num1 > num2}");
+  print("$num1 < $num2:  ${num1 < num2}");
+  print("$num1 == $num2:  ${num1 == num2}");
+  print("$num1 != $num2:  ${num1 != num2}");
+}
